74da0b70a4e75aa3329ecde47bebd62637665dac
miyamarinova <miyamarinova@gmail.com>
2024-06-25 00:10:53 -0400

    change DEBUG to false

--- bgfreshnet/settings.py
+++ bgfreshnet/settings.py
@@ -8,8 +8,8 @@ import certifi, os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-#DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
+#DEBUG = True
 if DEBUG:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
     CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://127.0.0.1']
